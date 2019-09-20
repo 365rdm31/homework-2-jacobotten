feb38fcf740b89edd387e27b26ee04b91c92e77f
Ryan Mersmann <rydmers@ksu.edu>
2019-09-19 19:05:19 -0500

    added Kelvin calculation to temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -4,3 +4,5 @@ echo "Enter a Farenheight temperature to convert to Celsius:"
 read tempf
 echo "Temp in degrees Celsius: "
 echo "scale=3; (($tempf-32.0)*(5.0/9.0))" | bc
+echo "Temp in degrees Kelvin: "
+echo "scale=3; (($tempf-32)*(5.0/9.0))+273.15" | bc
